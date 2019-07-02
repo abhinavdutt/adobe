5c2b34194aa036cc1baa2e69d2430a522a07e4ca
Abhinav Dutt <AbhinavD1@verifone.com>
2019-07-02 15:48:31 -0400

    New Changes

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+/*#################################################################
+#  main.cpp                                                     #
+#                                                               #
+#  Author: Abhinav Dutt                                         #
+#                                                               #
+#  main.cpp                                                     #
+#                                                               #
+#                                                               #
+#                                                               #
+#################################################################*/
+
 #include <atomic>
 #include <deque>
 #include <mutex>
@@ -9,20 +20,26 @@ using namespace std;
 
 int main()
 {
+
+	//Integer data type
 	MemoryPool<int> obj1(malloc, free);
-	auto p1 = obj1.alloc(100);
+	auto p1 = obj1.alloc(214);
 	cout<<p1<<endl;
 	obj1.free(p1);
 
+	// Double data type
 	MemoryPool<double> obj2(malloc, free);
 	auto p2 = obj2.alloc(100);
 	cout<<p2<<endl;
 	obj2.free(p2);
 
+	//Float data type
 	MemoryPool<float> obj3(malloc, free);
 	auto p3 = obj3.alloc(100.234567);
 	cout<<p3<<endl;
 	obj3.free(p3);
+
+
 
 	return 0;
 }
